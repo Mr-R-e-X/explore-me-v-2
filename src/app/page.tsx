80930841c796029dc268c13bba64871fa839453e
Souvik Hazra <souvikhazra151@gmail.com>
2025-12-22 12:38:13 +0530

    feat: vercal analytics use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Contact } from "@/components/custom/footer";
 import { Hero } from "@/components/custom/hero";
 import { Skill } from "@/components/custom/skills";
 
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white overflow-hidden">
